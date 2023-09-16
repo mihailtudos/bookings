39e8d4e9e7cdfe4798a12842970e79f37b8d3c1a
Mihail Tudos <mihailtudos@outlook.com>
2023-09-16 21:30:54 +0100

    added more advanced server side validation

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -1,8 +1,11 @@
 package forms
 
 import (
+	"fmt"
+	"github.com/asaskevich/govalidator"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 // Form creates a custom form struct, embeds an url.Values object
@@ -23,13 +26,52 @@ func New(data url.Values) *Form {
 func (f *Form) Has(field string, r *http.Request) bool {
 	x := r.Form.Get(field)
 	if len(x) == 0 {
-		f.Errors.Add(field, "This field is required.")
 		return false
 	}
 
 	return true
 }
 
+func (f *Form) Required(fields ...string) {
+	for _, field := range fields {
+		value := f.Get(field)
+		if len(strings.TrimSpace(value)) == 0 {
+			f.Errors.Add(field, "This field is required")
+		}
+	}
+}
+
 func (f *Form) Valid() bool {
 	return len(f.Errors) == 0
+}
+
+func (f *Form) MinLength(field string, length int, r *http.Request) bool {
+	x := r.Form.Get(field)
+
+	if len(x) < length {
+		f.Errors.Add(field, fmt.Sprintf("Must be at least %d characters long.", length))
+		return false
+	}
+
+	return true
+}
+
+func (f *Form) MaxLength(field string, length int, r *http.Request) bool {
+	x := r.Form.Get(field)
+
+	if len(x) > length {
+		f.Errors.Add(field, fmt.Sprintf("Must be max %d characters long.", length))
+		return false
+	}
+
+	return true
+}
+
+func (f *Form) IsEmail(field string) bool {
+	if !govalidator.IsEmail(f.Get(field)) {
+		f.Errors.Add(field, "Invalid email address.")
+		return false
+	}
+
+	return true
 }
