f1aa5d77c5c4a17e3965a6532e20abc3e35051f1
Mihail Tudos <mihailtudos@outlook.com>
2023-09-16 22:03:49 +0100

    showing error notification on session flash and clearing the session

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -141,9 +141,12 @@ func (m *Repository) ReservationSummary(w http.ResponseWriter, r *http.Request) 
 
 	if !ok {
 		log.Println("Cannot get item from session.")
+		m.App.Session.Put(r.Context(), "error", "Cannot get reservation from session.")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
 	}
 
+	m.App.Session.Remove(r.Context(), "reservation")
 	data := make(map[string]interface{})
 	data["reservation"] = reservation
 	render.RenderTemplate(w, r, "reservation-summary.page.gohtml", &models.TemplateData{
