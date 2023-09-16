6cc7b4a8c7b7089f42d79ee09ac653738eead60b
Mihail Tudos <mihailtudos@outlook.com>
2023-09-16 23:31:37 +0100

    tested 79 of the app

---
+++ cmd/web/routes_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/mihailtudos/bookings/internal/config"
+	"testing"
+)
+
+func TestRoutes(t *testing.T) {
+	var app config.AppConfig
+	mux := routes(&app)
+
+	switch v := mux.(type) {
+	case *chi.Mux:
+	default:
+		t.Error(fmt.Sprintf("Type is not *chi.Mux, type is %T", v))
+	}
+}
